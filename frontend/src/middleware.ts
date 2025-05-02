2a3b60f5b70e0e8d9dcac07ade054995dcae22ff
Vuong Tien Nguyen Bui <vuongtiennguyenbui@gmail.com>
2025-05-02 20:09:23 +0700

    fix: small fix

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
   const accessToken = req.cookies.get('authorization')?.value;
-=
 
   if (accessToken && path.startsWith('/api')) {
     const headers = new Headers(req.headers);
